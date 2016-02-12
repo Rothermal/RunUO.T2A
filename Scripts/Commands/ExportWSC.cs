ac72181cd9314f8fcf1bd412eedf010efdb09023
Grimoric <grimoric@hotmail.com>
2016-02-12 15:37:33 +0100

    Removed Items that is not used. They are suppsed to be added by the static class and dont have their own classes.

--- Scripts/Commands/ExportWSC.cs
+++ Scripts/Commands/ExportWSC.cs
@@ -24,7 +24,7 @@ namespace Server.Commands
 
 			foreach ( Item item in World.Items.Values )
 			{
-				if ( ( item is Static || item is BaseFloor || item is BaseWall )
+				if ( ( item is Static )
 					&& item.RootParent == null )
 				{
 					w.WriteLine( "SECTION WORLDITEM {0}", count );
--- Scripts/Items/Construction/Tables/Tables.cs
+++ Scripts/Items/Construction/Tables/Tables.cs
@@ -1,64 +1,5 @@
 namespace Server.Items
 {
-
-    [Furniture]
-	public class ElegantLowTable : Item
-	{
-		[Constructable]
-		public ElegantLowTable() : base(0x2819)
-		{
-			Weight = 1.0;
-		}
-
-		public ElegantLowTable(Serial serial) : base(serial)
-		{
-		}
-
-		public override void Serialize(GenericWriter writer)
-		{
-			base.Serialize(writer);
-
-			writer.Write((int) 0);
-		}
-
-		public override void Deserialize(GenericReader reader)
-		{
-			base.Deserialize(reader);
-
-			int version = reader.ReadInt();
-
-		}
-	}
-
-	[Furniture]
-	public class PlainLowTable : Item
-	{
-		[Constructable]
-		public PlainLowTable() : base(0x281A)
-		{
-			Weight = 1.0;
-		}
-
-		public PlainLowTable(Serial serial) : base(serial)
-		{
-		}
-
-		public override void Serialize(GenericWriter writer)
-		{
-			base.Serialize(writer);
-
-			writer.Write((int) 0);
-		}
-
-		public override void Deserialize(GenericReader reader)
-		{
-			base.Deserialize(reader);
-
-			int version = reader.ReadInt();
-
-		}
-	}
-
 	[Furniture]
 	[Flipable(0xB90,0xB7D)]
 	public class LargeTable : Item
@@ -154,4 +95,36 @@ namespace Server.Items
 				Weight = 1.0;
 		}
 	}
+
+    [Furniture]
+    [Flipable(0xB4A, 0xB49, 0xB4B, 0xB4C)]
+    public class WritingTable : Item
+    {
+        [Constructable]
+        public WritingTable() : base(0xB4A)
+        {
+            Weight = 1.0;
+        }
+
+        public WritingTable(Serial serial) : base(serial)
+        {
+        }
+
+        public override void Serialize(GenericWriter writer)
+        {
+            base.Serialize(writer);
+
+            writer.Write((int)0);
+        }
+
+        public override void Deserialize(GenericReader reader)
+        {
+            base.Deserialize(reader);
+
+            int version = reader.ReadInt();
+
+            if (Weight == 4.0)
+                Weight = 1.0;
+        }
+    }
 }
