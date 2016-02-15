5c36f091e5a40fd29f9719f5121511c3e069c737
Grimoric <grimoric@hotmail.com>
2016-02-15 17:45:42 +0100

    Updated Character Creation.

--- Scripts/Misc/CharacterCreation.cs
+++ Scripts/Misc/CharacterCreation.cs
@@ -10,7 +10,6 @@ namespace Server.Misc
 	{
 		public static void Initialize()
 		{
-			// Register our event handler
 			EventSink.CharacterCreated += new CharacterCreatedEventHandler( EventSink_CharacterCreated );
 		}
 
@@ -27,308 +26,9 @@ namespace Server.Misc
 			}
 
 			PackItem( new RedBook( "a book", m.Name, 20, true ) );
-			PackItem( new Gold( 1000 ) ); // Starting gold can be customized here
+			PackItem( new Gold( 100 ) );
 			PackItem( new Dagger() );
 			PackItem( new Candle() );
-		}
-
-		private static Item MakeNewbie( Item item )
-		{
-			item.LootType = LootType.Newbied;
-			return item;
-		}
-
-		private static void PlaceItemIn( Container parent, int x, int y, Item item )
-		{
-			parent.AddItem( item );
-			item.Location = new Point3D( x, y, 0 );
-		}
-
-		private static Item MakePotionKeg( PotionEffect type, int hue )
-		{
-			PotionKeg keg = new PotionKeg();
-
-			keg.Held = 100;
-			keg.Type = type;
-			keg.Hue = hue;
-
-			return MakeNewbie( keg );
-		}
-
-		private static void FillBankAOS( Mobile m )
-		{
-			BankBox bank = m.BankBox;
-			m.StatCap = 250;
-
-			Container cont;
-
-
-			// Begin box of money
-			cont = new WoodenBox();
-			cont.ItemID = 0xE7D;
-			cont.Hue = 0x489;
-
-			PlaceItemIn( cont, 16, 51, new BankCheck( 500000 ) );
-			PlaceItemIn( cont, 28, 51, new BankCheck( 250000 ) );
-			PlaceItemIn( cont, 40, 51, new BankCheck( 100000 ) );
-			PlaceItemIn( cont, 52, 51, new BankCheck( 100000 ) );
-			PlaceItemIn( cont, 64, 51, new BankCheck(  50000 ) );
-
-			PlaceItemIn( cont, 16, 115, new Silver( 9000 ) );
-			PlaceItemIn( cont, 34, 115, new Gold( 60000 ) );
-
-			PlaceItemIn( bank, 18, 169, cont );
-			// End box of money
-
-
-			// Begin bag of potion kegs
-			cont = new Backpack();
-			cont.Name = "Various Potion Kegs";
-
-			PlaceItemIn( cont,  45, 149, MakePotionKeg( PotionEffect.CureGreater, 0x2D ) );
-			PlaceItemIn( cont,  69, 149, MakePotionKeg( PotionEffect.HealGreater, 0x499 ) );
-			PlaceItemIn( cont,  93, 149, MakePotionKeg( PotionEffect.PoisonDeadly, 0x46 ) );
-			PlaceItemIn( cont, 117, 149, MakePotionKeg( PotionEffect.RefreshTotal, 0x21 ) );
-			PlaceItemIn( cont, 141, 149, MakePotionKeg( PotionEffect.ExplosionGreater, 0x74 ) );
-
-			PlaceItemIn( cont, 93, 82, new Bottle( 1000 ) );
-
-			PlaceItemIn( bank, 53, 169, cont );
-			// End bag of potion kegs
-
-
-			// Begin bag of tools
-			cont = new Bag();
-			cont.Name = "Tool Bag";
-
-			PlaceItemIn( cont, 30,  35, new TinkerTools( 1000 ) );
-			PlaceItemIn( cont, 90,  35, new DovetailSaw( 1000 ) );
-			PlaceItemIn( cont, 30,  68, new Scissors() );
-			PlaceItemIn( cont, 45,  68, new MortarPestle( 1000 ) );
-			PlaceItemIn( cont, 75,  68, new ScribesPen( 1000 ) );
-			PlaceItemIn( cont, 90,  68, new SmithHammer( 1000 ) );
-			PlaceItemIn( cont, 30, 118, new TwoHandedAxe() );
-			PlaceItemIn( cont, 60, 118, new FletcherTools( 1000 ) );
-			PlaceItemIn( cont, 90, 118, new SewingKit( 1000 ) );
-
-			PlaceItemIn( cont, 36, 51, new RunicHammer( CraftResource.DullCopper, 1000 ) );
-			PlaceItemIn( cont, 42, 51, new RunicHammer( CraftResource.ShadowIron, 1000 ) );
-			PlaceItemIn( cont, 48, 51, new RunicHammer( CraftResource.Copper, 1000 ) );
-			PlaceItemIn( cont, 54, 51, new RunicHammer( CraftResource.Bronze, 1000 ) );
-			PlaceItemIn( cont, 61, 51, new RunicHammer( CraftResource.Gold, 1000 ) );
-			PlaceItemIn( cont, 67, 51, new RunicHammer( CraftResource.Agapite, 1000 ) );
-			PlaceItemIn( cont, 73, 51, new RunicHammer( CraftResource.Verite, 1000 ) );
-			PlaceItemIn( cont, 79, 51, new RunicHammer( CraftResource.Valorite, 1000 ) );
-
-			PlaceItemIn( cont, 36, 55, new RunicSewingKit( CraftResource.SpinedLeather, 1000 ) );
-			PlaceItemIn( cont, 42, 55, new RunicSewingKit( CraftResource.HornedLeather, 1000 ) );
-			PlaceItemIn( cont, 48, 55, new RunicSewingKit( CraftResource.BarbedLeather, 1000 ) );
-
-			PlaceItemIn( bank, 118, 169, cont );
-			// End bag of tools
-
-
-			// Begin bag of archery ammo
-			cont = new Bag();
-			cont.Name = "Bag Of Archery Ammo";
-
-			PlaceItemIn( cont, 48, 76, new Arrow( 5000 ) );
-			PlaceItemIn( cont, 72, 76, new Bolt( 5000 ) );
-
-			PlaceItemIn( bank, 118, 124, cont );
-			// End bag of archery ammo
-
-
-			// Begin bag of treasure maps
-			cont = new Bag();
-			cont.Name = "Bag Of Treasure Maps";
-
-			PlaceItemIn( cont, 30, 35, new TreasureMap( 1, Map.Felucca ) );
-			PlaceItemIn( cont, 45, 35, new TreasureMap( 2, Map.Felucca ) );
-			PlaceItemIn( cont, 60, 35, new TreasureMap( 3, Map.Felucca ) );
-			PlaceItemIn( cont, 75, 35, new TreasureMap( 4, Map.Felucca ) );
-			PlaceItemIn( cont, 90, 35, new TreasureMap( 5, Map.Felucca ) );
-			PlaceItemIn( cont, 90, 35, new TreasureMap( 6, Map.Felucca ) );
-
-			PlaceItemIn( cont, 30, 50, new TreasureMap( 1, Map.Felucca ) );
-			PlaceItemIn( cont, 45, 50, new TreasureMap( 2, Map.Felucca ) );
-			PlaceItemIn( cont, 60, 50, new TreasureMap( 3, Map.Felucca ) );
-			PlaceItemIn( cont, 75, 50, new TreasureMap( 4, Map.Felucca ) );
-			PlaceItemIn( cont, 90, 50, new TreasureMap( 5, Map.Felucca ) );
-			PlaceItemIn( cont, 90, 50, new TreasureMap( 6, Map.Felucca ) );
-
-			PlaceItemIn( cont, 55, 100, new Lockpick( 30 ) );
-			PlaceItemIn( cont, 60, 100, new Pickaxe() );
-
-			PlaceItemIn( bank, 98, 124, cont );
-			// End bag of treasure maps
-
-
-			// Begin bag of raw materials
-			cont = new Bag();
-			cont.Hue = 0x835;
-			cont.Name = "Raw Materials Bag";
-
-			PlaceItemIn( cont, 92, 60, new BarbedLeather( 5000 ) );
-			PlaceItemIn( cont, 92, 68, new HornedLeather( 5000 ) );
-			PlaceItemIn( cont, 92, 76, new SpinedLeather( 5000 ) );
-			PlaceItemIn( cont, 92, 84, new Leather( 5000 ) );
-
-			PlaceItemIn( cont, 30, 118, new Cloth( 5000 ) );
-			PlaceItemIn( cont, 30,  84, new Board( 5000 ) );
-			PlaceItemIn( cont, 57,  80, new BlankScroll( 500 ) );
-
-			PlaceItemIn( cont, 30,  35, new DullCopperIngot( 5000 ) );
-			PlaceItemIn( cont, 37,  35, new ShadowIronIngot( 5000 ) );
-			PlaceItemIn( cont, 44,  35, new CopperIngot( 5000 ) );
-			PlaceItemIn( cont, 51,  35, new BronzeIngot( 5000 ) );
-			PlaceItemIn( cont, 58,  35, new GoldIngot( 5000 ) );
-			PlaceItemIn( cont, 65,  35, new AgapiteIngot( 5000 ) );
-			PlaceItemIn( cont, 72,  35, new VeriteIngot( 5000 ) );
-			PlaceItemIn( cont, 79,  35, new ValoriteIngot( 5000 ) );
-			PlaceItemIn( cont, 86,  35, new IronIngot( 5000 ) );
-
-			PlaceItemIn( bank, 98, 169, cont );
-			// End bag of raw materials
-
-
-			// Begin bag of spell casting stuff
-			cont = new Backpack();
-			cont.Hue = 0x480;
-			cont.Name = "Spell Casting Stuff";
-
-			PlaceItemIn( cont, 45, 105, new Spellbook( UInt64.MaxValue ) );
-
-			Runebook runebook = new Runebook( 10 );
-			runebook.CurCharges = runebook.MaxCharges;
-			PlaceItemIn( cont, 145, 105, runebook );
-
-			Item toHue = new BagOfReagents( 150 );
-			toHue.Hue = 0x2D;
-			PlaceItemIn( cont, 45, 150, toHue );
-
-			for ( int i = 0; i < 9; ++i )
-				PlaceItemIn( cont, 45 + (i * 10), 75, new RecallRune() );
-
-			PlaceItemIn( bank, 78, 169, cont );
-			// End bag of spell casting stuff
-
-
-			// Begin bag of ethereals
-			cont = new Backpack();
-			cont.Hue = 0x490;
-			cont.Name = "Bag Of Ethy's!";
-
-			PlaceItemIn( cont, 45, 66, new EtherealHorse() );
-			PlaceItemIn( cont, 69, 82, new EtherealOstard() );
-			PlaceItemIn( cont, 93, 99, new EtherealLlama() );
-			PlaceItemIn( cont, 117, 115, new EtherealKirin() );
-			PlaceItemIn( cont, 45, 132, new EtherealUnicorn() );
-			PlaceItemIn( cont, 69, 66, new EtherealRidgeback() );
-			PlaceItemIn( cont, 93, 82, new EtherealSwampDragon() );
-			PlaceItemIn( cont, 117, 99, new EtherealBeetle() );
-
-			PlaceItemIn( bank, 38, 124, cont );
-			// End bag of ethereals
-
-			for( int i = 0; i < 10; i++ )
-				PlaceItemIn( cont, 117, 128, new MessageInABottle( Map.Felucca, 4 ) );
-
-			PlaceItemIn( bank, 18, 124, cont );
-		}
-
-		private static void FillBankbox( Mobile m )
-		{
-			BankBox bank = m.BankBox;
-
-			bank.DropItem( new BankCheck( 1000000 ) );
-
-			// Full spellbook
-			Spellbook book = new Spellbook();
-
-			book.Content = ulong.MaxValue;
-
-			bank.DropItem( book );
-
-			Bag bag = new Bag();
-
-			for ( int i = 0; i < 5; ++i )
-				bag.DropItem( new Moonstone( MoonstoneType.Felucca ) );
-
-			// Felucca moonstones
-			bank.DropItem( bag );
-			
-			// Treasure maps
-			bank.DropItem( new TreasureMap( 1, Map.Felucca ) );
-			bank.DropItem( new TreasureMap( 2, Map.Felucca ) );
-			bank.DropItem( new TreasureMap( 3, Map.Felucca ) );
-			bank.DropItem( new TreasureMap( 4, Map.Felucca ) );
-			bank.DropItem( new TreasureMap( 5, Map.Felucca ) );
-
-			// Bag containing 50 of each reagent
-			bank.DropItem( new BagOfReagents( 50 ) );
-
-			// Craft tools
-			bank.DropItem( MakeNewbie( new Scissors() ) );
-			bank.DropItem( MakeNewbie( new SewingKit( 1000 ) ) );
-			bank.DropItem( MakeNewbie( new SmithHammer( 1000 ) ) );
-			bank.DropItem( MakeNewbie( new FletcherTools( 1000 ) ) );
-			bank.DropItem( MakeNewbie( new DovetailSaw( 1000 ) ) );
-			bank.DropItem( MakeNewbie( new MortarPestle( 1000 ) ) );
-			bank.DropItem( MakeNewbie( new ScribesPen( 1000 ) ) );
-			bank.DropItem( MakeNewbie( new TinkerTools( 1000 ) ) );
-
-			// A few dye tubs
-			bank.DropItem( new Dyes() );
-			bank.DropItem( new DyeTub() );
-			bank.DropItem( new DyeTub() );
-			bank.DropItem( new BlackDyeTub() );
-
-			DyeTub darkRedTub = new DyeTub();
-
-			darkRedTub.DyedHue = 0x485;
-			darkRedTub.Redyable = false;
-
-			bank.DropItem( darkRedTub );
-
-			// Some food
-			bank.DropItem( MakeNewbie( new Apple( 1000 ) ) );
-
-			// Resources
-			bank.DropItem( MakeNewbie( new Feather( 1000 ) ) );
-			bank.DropItem( MakeNewbie( new BoltOfCloth( 1000 ) ) );
-			bank.DropItem( MakeNewbie( new BlankScroll( 1000 ) ) );
-			bank.DropItem( MakeNewbie( new Hides( 1000 ) ) );
-			bank.DropItem( MakeNewbie( new Bandage( 1000 ) ) );
-			bank.DropItem( MakeNewbie( new Bottle( 1000 ) ) );
-			bank.DropItem( MakeNewbie( new Log( 1000 ) ) );
-
-			bank.DropItem( MakeNewbie( new IronIngot( 5000 ) ) );
-			bank.DropItem( MakeNewbie( new DullCopperIngot( 5000 ) ) );
-			bank.DropItem( MakeNewbie( new ShadowIronIngot( 5000 ) ) );
-			bank.DropItem( MakeNewbie( new CopperIngot( 5000 ) ) );
-			bank.DropItem( MakeNewbie( new BronzeIngot( 5000 ) ) );
-			bank.DropItem( MakeNewbie( new GoldIngot( 5000 ) ) );
-			bank.DropItem( MakeNewbie( new AgapiteIngot( 5000 ) ) );
-			bank.DropItem( MakeNewbie( new VeriteIngot( 5000 ) ) );
-			bank.DropItem( MakeNewbie( new ValoriteIngot( 5000 ) ) );
-
-			// Reagents
-			bank.DropItem( MakeNewbie( new BlackPearl( 1000 ) ) );
-			bank.DropItem( MakeNewbie( new Bloodmoss( 1000 ) ) );
-			bank.DropItem( MakeNewbie( new Garlic( 1000 ) ) );
-			bank.DropItem( MakeNewbie( new Ginseng( 1000 ) ) );
-			bank.DropItem( MakeNewbie( new MandrakeRoot( 1000 ) ) );
-			bank.DropItem( MakeNewbie( new Nightshade( 1000 ) ) );
-			bank.DropItem( MakeNewbie( new SulfurousAsh( 1000 ) ) );
-			bank.DropItem( MakeNewbie( new SpidersSilk( 1000 ) ) );
-
-			// Some extra starting gold
-			bank.DropItem( MakeNewbie( new Gold( 9000 ) ) );
-
-			// 5 blank recall runes
-			for ( int i = 0; i < 5; ++i )
-				bank.DropItem( MakeNewbie( new RecallRune() ) );
 		}
 
 		private static void AddShirt( Mobile m, int shirtHue )
@@ -408,28 +108,15 @@ namespace Server.Misc
 			newChar.Player = true;
 			newChar.AccessLevel = args.Account.AccessLevel;
 			newChar.Female = args.Female;
-			//newChar.Body = newChar.Female ? 0x191 : 0x190;
-
-			if( Core.Expansion >= args.Race.RequiredExpansion )
-				newChar.Race = args.Race;	//Sets body
-			else
-				newChar.Race = Race.DefaultRace;
-
-			//newChar.Hue = Utility.ClipSkinHue( args.Hue & 0x3FFF ) | 0x8000;
+			newChar.Race = Race.DefaultRace;
 			newChar.Hue = newChar.Race.ClipSkinHue( args.Hue & 0x3FFF ) | 0x8000;
 
 			newChar.Hunger = 20;
 
-			bool young = false;
-
 			if ( newChar is PlayerMobile )
 			{
 				PlayerMobile pm = (PlayerMobile) newChar;
-
 				pm.Profession = args.Profession;
-
-				if ( pm.AccessLevel == AccessLevel.Player && ((Account)pm.Account).Young )
-					young = pm.Young = true;
 			}
 
 			SetName( newChar, args.Name );
@@ -460,23 +147,15 @@ namespace Server.Misc
 				AddShoes( newChar );
 			}
 
-			if( TestCenter.Enabled )
-				FillBankbox( newChar );
+//			NewPlayerTicket ticket = new NewPlayerTicket();
+//			ticket.Owner = newChar;
+//			newChar.Backpack.DropItem( ticket );
 
-			if ( young )
-			{
-				NewPlayerTicket ticket = new NewPlayerTicket();
-				ticket.Owner = newChar;
-				newChar.BankBox.DropItem( ticket );
-			}
+            newChar.MoveToWorld(args.City.Location, args.City.Map);
 
-			CityInfo city = GetStartLocation( args, young );
-
-			newChar.MoveToWorld( city.Location, city.Map );
-
-			Console.WriteLine( "Login: {0}: New character being created (account={1})", state, args.Account.Username );
+            Console.WriteLine( "Login: {0}: New character being created (account={1})", state, args.Account.Username );
 			Console.WriteLine( " - Character: {0} (serial={1})", newChar.Name, newChar.Serial );
-			Console.WriteLine( " - Started: {0} {1} in {2}", city.City, city.Location, city.Map.ToString() );
+			Console.WriteLine( " - Started: {0} {1} in {2}", args.City.City, args.City.Location, args.City.Map.ToString() );
 
 			new WelcomeTimer( newChar ).Start();
 		}
@@ -506,74 +185,6 @@ namespace Server.Misc
 			{
 				m_Mobile.SendLocalizedMessage( m_Message );
 			}
-		}
-
-		private static readonly CityInfo m_NewHavenInfo = new CityInfo( "New Haven", "The Bountiful Harvest Inn", 3503, 2574, 14, Map.Felucca );
-
-		private static CityInfo GetStartLocation( CharacterCreatedEventArgs args, bool isYoung )
-		{
-			bool useHaven = isYoung;
-
-			ClientFlags flags = args.State == null ? ClientFlags.None : args.State.Flags;
-			Mobile m = args.Mobile;
-
-			switch ( args.Profession )
-			{
-                case 4: //Necro
-                {
-                    useHaven = true;
-                    
-                    new BadStartMessage(m, 1062205);
-                    /*
-					* Unfortunately you are playing on a *NON-Age-Of-Shadows* game 
-					* installation and cannot be transported to Malas.  
-					* You will not be able to take your new player quest in Malas 
-					* without an AOS client.  You are now being taken to the city of 
-					* Haven on the Trammel facet.
-					* */
-
-                    break;
-                }
-                case 5:	//Paladin
-				{
-					return m_NewHavenInfo;
-				}
-				case 6:	//Samurai
-				{
-					useHaven = true;
-                    
-					new BadStartMessage( m, 1063487 );
-					/*
-					* Unfortunately you are playing on a *NON-Samurai-Empire* game 
-					* installation and cannot be transported to Tokuno. 
-					* You will not be able to take your new player quest in Tokuno 
-					* without an SE client. You are now being taken to the city of 
-					* Haven on the Trammel facet.
-				    * */
-
-					break;
-				}
-				case 7:	//Ninja
-				{
-					useHaven = true;
-
-					new BadStartMessage( m, 1063487 );
-					/*
-					* Unfortunately you are playing on a *NON-Samurai-Empire* game 
-					* installation and cannot be transported to Tokuno. 
-					* You will not be able to take your new player quest in Tokuno 
-					* without an SE client. You are now being taken to the city of 
-					* Haven on the Trammel facet.
-					* */
-
-					break;
-				}
-			}
-
-			if( useHaven )
-				return m_NewHavenInfo;
-			else
-				return args.City;
 		}
 
 		private static void FixStats( ref int str, ref int dex, ref int intel, int max )
@@ -712,47 +323,6 @@ namespace Server.Misc
 
 					break;
 				}
-				case 4: // Necromancer
-				{
-					skills = new SkillNameValue[]
-						{
-							new SkillNameValue( SkillName.SpiritSpeak, 30 ),
-							new SkillNameValue( SkillName.Swords, 30 ),
-							new SkillNameValue( SkillName.Tactics, 20 )
-						};
-
-					break;
-				}
-				case 5: // Paladin
-				{
-					skills = new SkillNameValue[]
-						{
-							new SkillNameValue( SkillName.Swords, 49 ),
-							new SkillNameValue( SkillName.Tactics, 30 )
-						};
-
-					break;
-				}
-				case 6:	//Samurai
-				{
-					skills = new SkillNameValue[]
-						{
-							new SkillNameValue( SkillName.Swords, 50 ),
-							new SkillNameValue( SkillName.Anatomy, 30 ),
-							new SkillNameValue( SkillName.Healing, 30 )
-					};
-					break;
-				}
-				case 7:	//Ninja
-				{
-					skills = new SkillNameValue[]
-						{
-							new SkillNameValue( SkillName.Hiding, 50 ),
-							new SkillNameValue( SkillName.Fencing, 30 ),
-							new SkillNameValue( SkillName.Stealth, 30 )
-						};
-					break;
-				}
 				default:
 				{
 					if ( !ValidSkills( skills ) )
@@ -772,23 +342,6 @@ namespace Server.Misc
 					EquipItem( new LeatherChest() );
                     break;
 
-				}
-				case 4: // Necromancer
-				{
-					break;
-				}
-				case 5: // Paladin
-				{
-					break;
-				}
-
-				case 6: // Samurai
-				{
-					break;
-				}
-				case 7: // Ninja
-				{
-					break;
 				}
 			}
 
@@ -885,13 +438,6 @@ namespace Server.Misc
 				case 22: PackItem( new UnlockScroll() ); break;
 				case 23: PackItem( new WallOfStoneScroll() ); break;
 			}
-		}
-
-		private static Item NecroHue( Item item )
-		{
-			item.Hue = 0x2C3;
-
-			return item;
 		}
 
 		private static void AddSkillItems( SkillName skill, Mobile m )
